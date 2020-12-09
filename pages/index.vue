0b3f936ff4e430bde7d83daa6bf1130600bc68b6
Edenn Touitou <deozza@gmail.com>
2020-12-09 01:01:04 +0100

    added style for container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="horizontal-menu-content">
     <HorizontalNavbar
       theme="dark"
       :links="menuLinks"
--- pages/verticalNavbar.vue
+++ pages/verticalNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div >
+  <div class="vertical-menu-content">
     <VerticalNavbar
       theme="dark"
       :links="menuLinks"
